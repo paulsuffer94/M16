14f2e50bb35ccd2f80c24b7a7ab2371e7be612d8
Pavel Kirillov <pashakirillov94@gmail.com>
2022-11-26 18:38:21 +0300

    no message

---
+++ homework 16/homework 16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework 16
+//
+//  Created by macbook on 26.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
